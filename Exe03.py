90026a1c20d8967665d8229bb63d94acfc62365d
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-02-06 21:59:28 -0300

    MudançaExer03

--- Exe03.py
+++ Exe03.py
@@ -1,1 +1,7 @@
-# Exercicio 03
+# Faça um programa que receba um salario de 1500 e tenha um aumento de 5% do salario e o final exiba o salario com aumento
+print ("Voce ganhou 1500 , parabens")
+x = 1500
+z = x * 0.05
+x = x + z
+println ("")
+print (x)
