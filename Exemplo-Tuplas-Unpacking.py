7d2cf473132d47d85c004b99c35d556d7ecac9aa
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-02-19 19:18:38 -0300

    AA

---
+++ Exemplo-Tuplas-Unpacking.py
@@ -0,0 +1,7 @@
+vendas = ("Goti","14/05/22","14/06/08",2000, "Vadio")
+nome, data_contratacao, data_nas, salario, cargo = vendas
+print (nome)
+print (data_contratacao)
+print (data_nas)
+print (salario)
+print (cargo)
